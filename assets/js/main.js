77669b0c57861d6dbe84e0eb541e05c644923373
nirgn975 <nirgn975@gmail.com>
2016-02-14 00:05:41 +0200

    Used mailto for sending emails.

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,10 +15,10 @@ $(function (){
 
   $('#send-email').on('click', function() {
     // Get all the fields.
-    var first_name = $('#first_name').text();
-    var last_name = $('#last_name').text();
-    var email = $('#email').text();
-    var body = $('#textarea1').text();
+    var first_name = $('#first_name').val();
+    var last_name = $('#last_name').val();
+    var email = $('#email').val();
+    var body = $('#textarea1').val();
     // Check if the one of the fields in invalid.
     var email_has_invalid = $('#email').hasClass('invalid') || $('#textarea1').hasClass('invalid');
     var $toastContent;
@@ -26,7 +26,12 @@ $(function (){
     if (email_has_invalid) {
       $toastContent = $("<span>Can't send the email please fix the marked field.</span>");
     } else {
-      $toastContent = $("<span>Email send successfully.</span>");
+      // Get the Email subject ready.
+      var mail_subject = "subject=From your resume website";
+      var mail_body = "body=Hi,<br />My name is " + first_name + " "  + last_name + ".<br />I wanted to talk with you about:<br />\"\"\"<br />" + body + "<br />\"\"\"<br /><br />My Email is: " + email;
+      // Get the elemnt and insert the data.
+      var mail_to_link = $('#send-email');
+      mail_to_link.attr('href', 'mailto:nirgn975@gmail.com?' + mail_subject + '&' + mail_body);
     }
 
     Materialize.toast($toastContent, 5000);
