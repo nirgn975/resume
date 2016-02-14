01ac8c0172f1979095b844bfc3a5dde1ee031af1
nirgn975 <nirgn975@gmail.com>
2016-02-14 22:26:47 +0200

    edit general and skills text, change sass main file name, concat and uglify and sync javascript

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,18 +1,9 @@
 $(function (){
   "use strict";
 
-  // var options = [
-  //   {selector: '#general', offset: 0, callback: 'Materialize.showStaggeredList("#general")' },
-  //   {selector: '#work', offset: 0, callback: 'Materialize.showStaggeredList("#work")' },
-  //   {selector: '#eduction', offset: 0, callback: 'Materialize.showStaggeredList("#eduction")' },
-  //   {selector: '#skills', offset: 0, callback: 'Materialize.showStaggeredList("#skills")' },
-  //   {selector: '#projects', offset: 0, callback: 'Materialize.showStaggeredList("#projects")' },
-  //   {selector: '#contact', offset: 0, callback: 'Materialize.showStaggeredList("#contact")' },
-  // ];
-  // Materialize.scrollFire(options);
-
-  $(".button-collapse").sideNav();
-
+  /**
+   *  Handle the Send email button, on contact section.
+   */
   $('#send-email').on('click', function() {
     // Get all the fields.
     var first_name = $('#first_name').val();
@@ -21,10 +12,10 @@ $(function (){
     var body = $('#textarea1').val();
     // Check if the one of the fields in invalid.
     var email_has_invalid = $('#email').hasClass('invalid') || $('#textarea1').hasClass('invalid');
-    var $toastContent;
 
     if (email_has_invalid) {
-      $toastContent = $("<span>Can't send the email please fix the marked field.</span>");
+      var toastContent = $("<span>Can't send the email please fix the marked field.</span>");
+      Materialize.toast(toastContent, 5000);
     } else {
       // Get the Email subject ready.
       var mail_subject = "subject=From your resume website";
@@ -33,7 +24,5 @@ $(function (){
       var mail_to_link = $('#send-email');
       mail_to_link.attr('href', 'mailto:nirgn975@gmail.com?' + mail_subject + '&' + mail_body);
     }
-
-    Materialize.toast($toastContent, 5000);
   });
 });
