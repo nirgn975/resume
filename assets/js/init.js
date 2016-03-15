9f775e105cbd7d9dbe5afe6b7f88fa8922fffd6d
Nir Galon <nir@galons.io>
2016-03-16 00:36:18 +0200

    Edit text in education and projects sections. And add a 'show more' button for long eduction.

--- assets/js/init.js
+++ assets/js/init.js
@@ -3,4 +3,15 @@ $(function (){
 
   // Activatethe mobile side nav.
   $(".button-collapse").sideNav();
+
+
+  $('.education-description li').hide().filter(':lt(5)').show();
+  $('#toggle-courses a').click(function(){
+    $('.education-description li:gt(4)').toggle();
+    $('#education-description-hide-text').toggleClass('long-list');
+    var buttonText = $(this).text() === "Show More" ? "Show Less" : "Show More";
+    $(this).text(buttonText);
+  });
+
+
 });
