f7c9d33157482ebdcd0788c4dd59dc38deff93cd
Nir Galon <nir@galons.io>
2016-03-16 21:47:44 +0200

    finish 'show more' logic button.

--- assets/js/init.js
+++ assets/js/init.js
@@ -4,14 +4,22 @@ $(function (){
   // Activatethe mobile side nav.
   $(".button-collapse").sideNav();
 
-
+  /**
+   * Handle the "Show More" button in Education section.
+   */
   $('.education-description li').hide().filter(':lt(5)').show();
   $('#toggle-courses a').click(function(){
-    $('.education-description li:gt(4)').toggle();
+    // Check if the courses are open or close.
+    if ($('.education-description').hasClass('long-list')) {
+      $('.education-description li:gt(4)').slideDown('slow');
+    } else {
+      $('.education-description li:gt(4)').slideUp('slow');
+    }
+
     $('#education-description-hide-text').toggleClass('long-list');
+
+    // Chage the text, and set the appropriate one.
     var buttonText = $(this).text() === "Show More" ? "Show Less" : "Show More";
     $(this).text(buttonText);
   });
-
-
 });
