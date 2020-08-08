2e9de5b58914167704756a0ea5879151e28c26ee
Nir Galon <nir@galon.io>
2020-08-08 15:13:20 +0300

    Add work, education and posts basic layout

--- static/js/main.js
+++ static/js/main.js
@@ -2,10 +2,29 @@ $('.projects .card .image').dimmer({
   on: 'hover'
 });
 
-console.log('foo');
-const RSS_URL = `https://lifelongstudent.io/index.xml`;
+$(document).ready(function() {
+  // Get lates blog posts.
+  fetch('https://lifelongstudent.io/index.xml')
+    .then(response => response.text())
+    .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then(data => {
+      const channel = data.childNodes[0].childNodes[0];
 
-fetch(RSS_URL)
-  .then(response => response.text())
-  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-  .then(data => console.log(data))
+
+      for (let entity of channel.childNodes.values()) {
+        if (entity.nodeName == 'item') {
+          $('.posts').append(`<div class="four wide column">
+            <div class="ui card">
+              <div class="content">
+                <div class="header">${entity.getElementsByTagName("title")[0].textContent}</div>
+                <div class="meta">
+                  <span>${entity.getElementsByTagName("pubDate")[0].textContent.split(':')[0].slice(0, -2)}</span>
+                </div>
+                <p>${entity.getElementsByTagName("description")[0].textContent.replace('src="/', 'src="https://lifelongstudent.io/')}</p>
+              </div>
+            </div>
+          </div>`);
+        }
+      }
+    });
+});
